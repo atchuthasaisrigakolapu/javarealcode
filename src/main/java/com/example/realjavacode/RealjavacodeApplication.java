e340472cff4bf3915481a1a9e89f952738cbe17a
atchuthasaisrigakolapu <atchuthasaisrigakolapu@gmail.com>
2022-04-03 13:17:50 +0530

    First spring boot commit

---
+++ src/main/java/com/example/realjavacode/RealjavacodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.realjavacode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealjavacodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealjavacodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/realjavacode/RealjavacodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realjavacode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealjavacodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
